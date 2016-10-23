773c9f608f6df88523ca93d50a1d878aea6a4688
Akinwande Adegbola <akinwande.adegbola@interswitchgroup.com>
2016-10-23 18:39:28 +0100

    Merge

--- src/main/java/com/interswitch/techquest/secure/utils/InterswitchAuth.java
+++ src/main/java/com/interswitch/techquest/secure/utils/InterswitchAuth.java
@@ -7,13 +7,24 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.security.MessageDigest;
+import java.security.PublicKey;
+import java.security.SecureRandom;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TimeZone;
 import java.util.UUID;
 
+import javax.crypto.Cipher;
+
 import org.apache.commons.codec.binary.Base64;
+import org.bouncycastle.crypto.KeyGenerationParameters;
+import org.bouncycastle.crypto.engines.DESedeEngine;
+import org.bouncycastle.crypto.engines.RSAEngine;
+import org.bouncycastle.crypto.generators.DESedeKeyGenerator;
+import org.bouncycastle.crypto.params.DESedeParameters;
+import org.bouncycastle.crypto.params.RSAKeyParameters;
+import org.bouncycastle.util.encoders.Hex;
 import org.json.JSONObject;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -34,14 +45,15 @@ public class InterswitchAuth {
 	public static HashMap<String, String> generateInterswitchAuth(
 			String httpMethod, String resourceUrl, String clientId,
 			String clientSecretKey, String additionalParameters,
-			String signatureMethod,String authType,String environment) throws Exception {
+			String signatureMethod, String authType, String environment)
+			throws Exception {
 		HashMap<String, String> interswitchAuth = new HashMap<String, String>();
-		
-		//Timezone MUST be Africa/Lagos.
+
+		// Timezone MUST be Africa/Lagos.
 		TimeZone lagosTimeZone = TimeZone.getTimeZone("Africa/Lagos");
 
 		Calendar calendar = Calendar.getInstance(lagosTimeZone);
-		
+
 		// Timestamp must be in seconds.
 		long timestamp = calendar.getTimeInMillis() / 1000;
 
@@ -59,24 +71,24 @@ public class InterswitchAuth {
 				+ clientSecretKey;
 		if (additionalParameters != null && !"".equals(additionalParameters))
 			signatureCipher = signatureCipher + "&" + additionalParameters;
-		//System.out.println(signatureCipher);
+		// System.out.println(signatureCipher);
 		MessageDigest messageDigest = MessageDigest
 				.getInstance(signatureMethod);
 		byte[] signatureBytes = messageDigest
 				.digest(signatureCipher.getBytes());
-		
-		// encode signature as base 64 
+
+		// encode signature as base 64
 		String signature = new String(Base64.encodeBase64(signatureBytes));
-//		System.out.println("Cipher: " + signatureCipher);
-		if(authType.equalsIgnoreCase("oauth"))
-		{
-			String bearerAuthorization = getAccessToken(clientId, clientSecretKey, environment);
-			interswitchAuth.put(AUTHORIZATION, AUTHORIZATION_REALM_BEARER+" "+bearerAuthorization);
-		}else
-		{
+		// System.out.println("Cipher: " + signatureCipher);
+		if (authType.equalsIgnoreCase("oauth")) {
+			String bearerAuthorization = getAccessToken(clientId,
+					clientSecretKey, environment);
+			interswitchAuth.put(AUTHORIZATION, AUTHORIZATION_REALM_BEARER + " "
+					+ bearerAuthorization);
+		} else {
 			interswitchAuth.put(AUTHORIZATION, authorization);
 		}
-		
+
 		interswitchAuth.put(TIMESTAMP, String.valueOf(timestamp));
 		interswitchAuth.put(NONCE, nonce);
 		interswitchAuth.put(SIGNATURE_METHOD, signatureMethod);
@@ -84,37 +96,38 @@ public class InterswitchAuth {
 
 		return interswitchAuth;
 	}
-	
-	
-    public static String getAccessToken(String clientId, String clientSecret,String environment) throws Exception
-    {
-    	String PRODUCTION_PASSPORT_RESOURCE_URL = "https://passport.interswitchng.com/passport/oauth/token";
-    	String SANDBOX_PASSPORT_RESOURCE_URL = "http://sandbox.interswitchng.com/passport/oauth/token";
-    	String PASSPORT_RESOURCE_URL = "";
-    	
-    	if(environment.equalsIgnoreCase("live"))
-    	{
-    		PASSPORT_RESOURCE_URL = PRODUCTION_PASSPORT_RESOURCE_URL;
-    	}else
-    	{
-    		PASSPORT_RESOURCE_URL = SANDBOX_PASSPORT_RESOURCE_URL;
-    	}
-    	
-    	URL obj = new URL(PASSPORT_RESOURCE_URL);
-    	
+
+	public static String getAccessToken(String clientId, String clientSecret,
+			String environment) throws Exception {
+		String PRODUCTION_PASSPORT_RESOURCE_URL = "https://passport.interswitchng.com/passport/oauth/token";
+		String SANDBOX_PASSPORT_RESOURCE_URL = "http://sandbox.interswitchng.com/passport/oauth/token";
+		String PASSPORT_RESOURCE_URL = "";
+
+		if (environment.equalsIgnoreCase("live")) {
+			PASSPORT_RESOURCE_URL = PRODUCTION_PASSPORT_RESOURCE_URL;
+		} else {
+			PASSPORT_RESOURCE_URL = SANDBOX_PASSPORT_RESOURCE_URL;
+		}
+
+		URL obj = new URL(PASSPORT_RESOURCE_URL);
+
 		System.setProperty("http.maxRedirects", "100");
 		java.net.CookieManager cm = new java.net.CookieManager();
 		java.net.CookieHandler.setDefault(cm);
-		
-		System.out.println("\nSending 'POST' request to URL : " + PASSPORT_RESOURCE_URL);
+
+		System.out.println("\nSending 'POST' request to URL : "
+				+ PASSPORT_RESOURCE_URL);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 		con.setRequestMethod("POST");
 		String basicAuthCipher = clientId + ":" + clientSecret;
-		String basicAuth = new String(org.bouncycastle.util.encoders.Base64.encode(basicAuthCipher.getBytes()));
+		String basicAuth = new String(
+				org.bouncycastle.util.encoders.Base64.encode(basicAuthCipher
+						.getBytes()));
 		System.out.println("\nBasic Auth : " + basicAuth);
-		
-//    	// Set Headers
-		con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+		// // Set Headers
+		con.setRequestProperty("Content-Type",
+				"application/x-www-form-urlencoded");
 		con.setRequestProperty("Authorization", "Basic " + basicAuth);
 		String request = "grant_type=client_credentials";
 
@@ -124,36 +137,243 @@ public class InterswitchAuth {
 		wr.writeBytes(request);
 		wr.flush();
 		wr.close();
-    	    		
+
 		int responseCode = con.getResponseCode();
-		
-//		System.out.println("Post parameters : " + request);
+
+		// System.out.println("Post parameters : " + request);
 		System.out.println("Response Code : " + responseCode);
 		BufferedReader in = null;
-		try
-		{
-			in = new BufferedReader(new InputStreamReader(con.getInputStream()));        		
-		}
-		catch(Exception ex)
-		{
+		try {
+			in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		} catch (Exception ex) {
 			ex.printStackTrace();
-			in = new BufferedReader(new InputStreamReader(con.getErrorStream())); 
+			in = new BufferedReader(new InputStreamReader(con.getErrorStream()));
 		}
-		
+
 		String inputLine;
 		StringBuffer response = new StringBuffer();
 		while ((inputLine = in.readLine()) != null) {
 			response.append(inputLine);
 		}
-		in.close();    		    		
-		//print result
+		in.close();
+		// print result
 		JSONObject jSONObjectx = new JSONObject(response.toString());
 		System.out.println(jSONObjectx.toString(2));
-//		System.out.println(response.toString());
+		// System.out.println(response.toString());
 		ObjectMapper mapper = new ObjectMapper();
 		Map<String, String> map = new HashMap<String, String>();
-		map = mapper.readValue(response.toString(), new TypeReference<Map<String, String>>(){});
-		
-		return map.get("access_token");	
-    }
+		map = mapper.readValue(response.toString(),
+				new TypeReference<Map<String, String>>() {
+				});
+
+		return map.get("access_token");
+	}
+
+	public static String generateTransactionAuthData(String version, String pan,
+			String pin, String expiryDate, String cvv2) throws Exception {
+		String authData = "";
+		String authDataCipher = version + "Z" + pan + "Z" + pin + "Z"
+				+ expiryDate + "Z" + cvv2;
+		System.out.println("Auth Data Cipher: " + authDataCipher);
+
+		// The Modulus and Public Exponent will be supplied by Interswitch.
+		// please ask for one
+		String modulus = "9c7b3ba621a26c4b02f48cfc07ef6ee0aed8e12b4bd11c5cc0abf80d5206be69e1891e60fc88e2d565e2fabe4d0cf630e318a6c721c3ded718d0c530cdf050387ad0a30a336899bbda877d0ec7c7c3ffe693988bfae0ffbab71b25468c7814924f022cb5fda36e0d2c30a7161fa1c6fb5fbd7d05adbef7e68d48f8b6c5f511827c4b1c5ed15b6f20555affc4d0857ef7ab2b5c18ba22bea5d3a79bd1834badb5878d8c7a4b19da20c1f62340b1f7fbf01d2f2e97c9714a9df376ac0ea58072b2b77aeb7872b54a89667519de44d0fc73540beeaec4cb778a45eebfbefe2d817a8a8319b2bc6d9fa714f5289ec7c0dbc43496d71cf2a642cb679b0fc4072fd2cf";
+		String publicExponent = "010001";
+
+		PublicKey publicKey = Util.getPublicKey(modulus, publicExponent);
+		Cipher encryptCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding", "BC");
+		encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+		byte[] authDataBytes = encryptCipher.doFinal(authDataCipher
+				.getBytes("UTF8"));
+		authData = new String(
+				org.bouncycastle.util.encoders.Base64.encode(authDataBytes))
+				.trim();
+		return authData;
+	}
+
+	public static String getPINBlock(String pin, String cvv2, String expiryDate,
+			byte[] keyBytes) {
+		pin = null == pin || pin.equals("") ? "0000" : pin;
+		cvv2 = null == cvv2 || cvv2.equals("") ? "000" : cvv2;
+		expiryDate = null == expiryDate || expiryDate.equals("") ? "0000"
+				: expiryDate;
+
+		String pinBlockString = pin + cvv2 + expiryDate;
+		int pinBlockStringLen = pinBlockString.length();
+		String pinBlickLenLenString = String.valueOf(pinBlockStringLen);
+		int pinBlickLenLen = pinBlickLenLenString.length();
+		String clearPINBlock = String.valueOf(pinBlickLenLen)
+				+ pinBlockStringLen + pinBlockString;
+
+		byte randomBytes = 0x0;
+		int randomDigit = (int) ((randomBytes * 10) / 128);
+		randomDigit = Math.abs(randomDigit);
+		int pinpadlen = 16 - clearPINBlock.length();
+		for (int i = 0; i < pinpadlen; i++) {
+			clearPINBlock = clearPINBlock + randomDigit;
+		}
+
+		DESedeEngine engine = new DESedeEngine();
+		DESedeParameters keyParameters = new DESedeParameters(keyBytes);
+		engine.init(true, keyParameters);
+		byte[] clearPINBlockBytes = Hex.decode(clearPINBlock);
+		byte[] encryptedPINBlockBytes = new byte[8];
+		engine.processBlock(clearPINBlockBytes, 0, encryptedPINBlockBytes, 0);
+		byte[] encodedEncryptedPINBlockBytes = Hex
+				.encode(encryptedPINBlockBytes);
+		String pinBlock = new String(encodedEncryptedPINBlockBytes);
+
+		pin = "0000000000000000";
+		clearPINBlock = "0000000000000000";
+
+		Util.zeroise(clearPINBlockBytes);
+		Util.zeroise(encryptedPINBlockBytes);
+		Util.zeroise(encodedEncryptedPINBlockBytes);
+
+		return pinBlock;
+	}
+
+	public String getSecure(String publicKeyModulus, String publicKeyExponent,
+			byte[] keyBytes, String pan) {
+		byte[] secureBytes = new byte[64];
+		byte[] headerBytes = new byte[1];
+		byte[] formatVersionBytes = new byte[1];
+		byte[] macVersionBytes = new byte[1];
+		byte[] pinDesKey = new byte[16];
+		byte[] macDesKey = new byte[16];
+		byte[] macBytes = new byte[4];
+		byte[] customerIdBytes = new byte[10];
+		byte[] footerBytes = new byte[1];
+		byte[] otherBytes = new byte[14];
+
+		System.arraycopy(customerIdBytes, 0, secureBytes, 35, 10);
+		System.arraycopy(macBytes, 0, secureBytes, 45, 4);
+		System.arraycopy(otherBytes, 0, secureBytes, 49, 14);
+		System.arraycopy(footerBytes, 0, secureBytes, 63, 1);
+
+		headerBytes = Util.HexConverter("4D");
+		formatVersionBytes = Util.HexConverter("10");
+		macVersionBytes = Util.HexConverter("10");
+
+		pinDesKey = keyBytes;
+
+		if (pan != null) {
+			int panDiff = 20 - pan.length();
+			String panString = panDiff + pan;
+			int panlen = 20 - panString.length();
+			for (int i = 0; i < panlen; i++) {
+				panString += "F";
+			}
+
+			customerIdBytes = Util.HexConverter(Util.padRight(panString, 20));
+
+		}
+
+		footerBytes = Util.HexConverter("5A");
+
+		System.arraycopy(headerBytes, 0, secureBytes, 0, 1);
+		System.arraycopy(formatVersionBytes, 0, secureBytes, 1, 1);
+		System.arraycopy(macVersionBytes, 0, secureBytes, 2, 1);
+		System.arraycopy(pinDesKey, 0, secureBytes, 3, 16);
+		System.arraycopy(macDesKey, 0, secureBytes, 19, 16);
+		System.arraycopy(customerIdBytes, 0, secureBytes, 35, 10);
+		System.arraycopy(macBytes, 0, secureBytes, 45, 4);
+		System.arraycopy(otherBytes, 0, secureBytes, 49, 14);
+		System.arraycopy(footerBytes, 0, secureBytes, 63, 1);
+
+		RSAEngine engine = new RSAEngine();
+		RSAKeyParameters publicKeyParameters = Util.getPublicSecureKey(
+				publicKeyModulus, publicKeyExponent);
+		engine.init(true, publicKeyParameters);
+		byte[] encryptedSecureBytes = engine.processBlock(secureBytes, 0,
+				secureBytes.length);
+		byte[] encodedEncryptedSecureBytes = Hex.encode(encryptedSecureBytes);
+		String encrytedSecure = new String(encodedEncryptedSecureBytes);
+
+		Util.zeroise(secureBytes);
+
+		return encrytedSecure;
+	}
+
+	public static String getSecureV2(String publicKeyModulus,
+			String publicKeyExponent, byte[] keyBytes, String pan,
+			String susbscriberID, String transCode, String amt,
+			String phoneNum, String custNum, String paymentItemCode,
+			String cardName) {
+		byte[] secureBytes = new byte[64];
+		byte[] headerBytes = new byte[1];
+		byte[] formatVersionBytes = new byte[1];
+		byte[] macVersionBytes = new byte[1];
+		byte[] pinDesKey = new byte[16];
+		byte[] macDesKey = new byte[16];
+		byte[] macBytes = new byte[4];
+		byte[] customerIdBytes = new byte[10];
+		byte[] footerBytes = new byte[1];
+		byte[] otherBytes = new byte[14];
+
+		System.arraycopy(customerIdBytes, 0, secureBytes, 35, 10);
+		System.arraycopy(macBytes, 0, secureBytes, 45, 4);
+		System.arraycopy(otherBytes, 0, secureBytes, 49, 14);
+		System.arraycopy(footerBytes, 0, secureBytes, 63, 1);
+
+		headerBytes = Util.HexConverter("4D");
+		formatVersionBytes = Util.HexConverter("10");
+		macVersionBytes = Util.HexConverter("10");
+
+		pinDesKey = keyBytes;
+
+		if (pan != null || pan != "") {
+			int panDiff = 20 - pan.length();
+			String panString = panDiff + pan;
+			int panlen = 20 - panString.length();
+			for (int i = 0; i < panlen; i++) {
+				panString += "F";
+			}
+
+			customerIdBytes = Util.HexConverter(Util.padRight(panString, 20));
+
+		}
+
+		String macData = Util.getMACDataVersion9(susbscriberID, cardName,
+				transCode, amt, phoneNum, custNum, paymentItemCode);
+		macBytes = Hex.decode(Util.getMAC(macData, macDesKey, 11));
+		footerBytes = Util.HexConverter("5A");
+
+		System.arraycopy(headerBytes, 0, secureBytes, 0, 1);
+		System.arraycopy(formatVersionBytes, 0, secureBytes, 1, 1);
+		System.arraycopy(macVersionBytes, 0, secureBytes, 2, 1);
+		System.arraycopy(pinDesKey, 0, secureBytes, 3, 16);
+		System.arraycopy(macDesKey, 0, secureBytes, 19, 16);
+		System.arraycopy(customerIdBytes, 0, secureBytes, 35, 10);
+		System.arraycopy(macBytes, 0, secureBytes, 45, 4);
+		System.arraycopy(otherBytes, 0, secureBytes, 49, 14);
+		System.arraycopy(footerBytes, 0, secureBytes, 63, 1);
+
+		RSAEngine engine = new RSAEngine();
+		RSAKeyParameters publicKeyParameters = Util.getPublicSecureKey(
+				publicKeyModulus, publicKeyExponent);
+		engine.init(true, publicKeyParameters);
+		byte[] encryptedSecureBytes = engine.processBlock(secureBytes, 0,
+				secureBytes.length);
+		byte[] encodedEncryptedSecureBytes = Hex.encode(encryptedSecureBytes);
+		String encrytedSecure = new String(encodedEncryptedSecureBytes);
+
+		Util.zeroise(secureBytes);
+
+		return encrytedSecure;
+	}
+
+	public static byte[] generateKey() {
+		SecureRandom sr = new SecureRandom();
+		KeyGenerationParameters kgp = new KeyGenerationParameters(sr,
+				DESedeParameters.DES_KEY_LENGTH * 16);
+		DESedeKeyGenerator kg = new DESedeKeyGenerator();
+		kg.init(kgp);
+
+		byte[] desKeyBytes = kg.generateKey();
+		DESedeParameters.setOddParity(desKeyBytes);
+
+		return desKeyBytes;
+	}
 }
---
+++ src/main/java/com/interswitch/techquest/secure/utils/Util.java
@@ -0,0 +1,164 @@
+package com.interswitch.techquest.secure.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.Security;
+import java.security.spec.RSAPublicKeySpec;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.bouncycastle.crypto.engines.DESedeEngine;
+import org.bouncycastle.crypto.macs.CBCBlockCipherMac;
+import org.bouncycastle.crypto.params.DESedeParameters;
+import org.bouncycastle.crypto.params.RSAKeyParameters;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Hex;
+
+public class Util {
+
+	public static PublicKey getPublicKey(String modulus, String publicExponent)
+			throws Exception {
+		Security.addProvider(new BouncyCastleProvider());
+		RSAPublicKeySpec publicKeyspec = new RSAPublicKeySpec(new BigInteger(
+				modulus, 16), new BigInteger(publicExponent, 16));
+		KeyFactory factory = KeyFactory.getInstance("RSA");
+		PublicKey publicKey = factory.generatePublic(publicKeyspec);
+		return publicKey;
+	}
+
+	public static String getMACDataVersion9(String susbscriberID,
+			String cardName, String transCode, String amt, String phoneNum,
+			String custNum, String paymentItemCode) {
+		String macData = "";
+		if (susbscriberID != null && !susbscriberID.isEmpty()) {
+			macData = macData + susbscriberID;
+		}
+		if (cardName != null && !cardName.isEmpty()) {
+			macData = macData + cardName;
+		}
+		if (transCode != null && !transCode.isEmpty()) {
+			macData = macData + transCode;
+		}
+		if (amt != null && !amt.isEmpty()) {
+			macData = macData + amt;
+		}
+		if (phoneNum != null && !phoneNum.isEmpty()) {
+			macData = macData + phoneNum;
+		}
+		if (custNum != null && !custNum.isEmpty()) {
+			macData = macData + custNum;
+		}
+		if (paymentItemCode != null && !paymentItemCode.isEmpty()) {
+			macData = macData + paymentItemCode;
+		}
+		return macData;
+	}
+
+	public static RSAKeyParameters getPublicSecureKey(String modulus,
+			String exponent) {
+		BigInteger modulusByte = new BigInteger(Hex.decode(modulus));
+		BigInteger exponentByte = new BigInteger(Hex.decode(exponent));
+		RSAKeyParameters pkParameters = new RSAKeyParameters(false,
+				modulusByte, exponentByte);
+		return pkParameters;
+	}
+
+	public static byte[] HexConverter(String str) {
+		try {
+			str = new String(str.getBytes(), StandardCharsets.UTF_8);
+			byte[] myBytes = Hex.decode(str);
+			return myBytes;
+		} catch (Exception ex) {
+			Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return null;
+	}
+
+	public static String padRight(String data, int maxLen) {
+
+		if (data == null || data.length() >= maxLen)
+			return data;
+
+		int len = data.length();
+		int deficitLen = maxLen - len;
+		for (int i = 0; i < deficitLen; i++)
+			data += "0";
+
+		return data;
+	}
+
+	public static void zeroise(byte[] data) {
+		int len = data.length;
+
+		for (int i = 0; i < len; i++)
+			data[i] = 0;
+	}
+
+	public static String getMAC(String macData, byte[] macKey, int macVersion) {
+		byte[] macBytes = new byte[4];
+		byte[] macDataBytes = macData.getBytes();
+		byte[] encodedMacBytes;
+		String macCipher1;
+		SecretKeySpec keyParameters1;
+		Mac engine1;
+		if (macVersion == 8) {
+			macCipher1 = "";
+
+			try {
+				keyParameters1 = new SecretKeySpec(macKey, "HmacSHA1");
+				engine1 = Mac.getInstance(keyParameters1.getAlgorithm());
+				engine1.init(keyParameters1);
+				encodedMacBytes = macData.getBytes();
+				macBytes = engine1.doFinal(encodedMacBytes);
+				macCipher1 = new String(Hex.encode(macBytes), "UTF-8");
+			} catch (InvalidKeyException var11) {
+				;
+			} catch (NoSuchAlgorithmException var12) {
+				;
+			} catch (UnsupportedEncodingException var13) {
+				;
+			}
+
+			return macCipher1;
+		} else if (macVersion == 12) {
+			macCipher1 = "";
+
+			try {
+				keyParameters1 = new SecretKeySpec(macKey, "HmacSHA256");
+				engine1 = Mac.getInstance(keyParameters1.getAlgorithm());
+				engine1.init(keyParameters1);
+				encodedMacBytes = macData.getBytes();
+				macBytes = engine1.doFinal(encodedMacBytes);
+				macCipher1 = new String(Hex.encode(macBytes), "UTF-8");
+			} catch (InvalidKeyException var14) {
+				;
+			} catch (NoSuchAlgorithmException var15) {
+				;
+			} catch (UnsupportedEncodingException var16) {
+				;
+			}
+
+			return macCipher1;
+		} else {
+			CBCBlockCipherMac macCipher = new CBCBlockCipherMac(
+					new DESedeEngine());
+			DESedeParameters keyParameters = new DESedeParameters(macKey);
+			DESedeEngine engine = new DESedeEngine();
+			engine.init(true, keyParameters);
+			macCipher.init(keyParameters);
+			macCipher.update(macDataBytes, 0, macData.length());
+			macCipher.doFinal(macBytes, 0);
+			encodedMacBytes = Hex.encode(macBytes);
+			String mac = new String(encodedMacBytes);
+			return mac;
+		}
+	}
+}
